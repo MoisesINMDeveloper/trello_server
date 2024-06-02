cb6eb1537c43d0eb5c3cee2028144e8130ad2c60
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2024-06-02 12:05:37 -0400

    Configuracion de JWT

--- dist/services/auth.service.js
+++ dist/services/auth.service.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateToken = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const JWT_SECRET = process.env.JWT_SECRET || "defautl-secret";
+const JWT_SECRET = process.env.JWT_SECRET || "default-secret";
 const generateToken = (user) => {
     return jsonwebtoken_1.default.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-        expiresIn: "1h",
+        expiresIn: "7d",
     });
 };
 exports.generateToken = generateToken;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 import { User } from "../types/user.interface";
-const JWT_SECRET = process.env.JWT_SECRET || "defautl-secret";
+const JWT_SECRET = process.env.JWT_SECRET || "default-secret";
 
 export const generateToken = (user: User): string => {
   return jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "7d",
   });
 };
